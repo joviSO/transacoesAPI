cc56830a79adcd6d4b3d56be0fc66eb6f3e2ca77
João <levelupdojoao@hotmail.com>
2024-05-27 21:25:09 -0300

    update/Gems_update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_000532) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_001429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_000532) do
     t.bigint "user_id", null: false
     t.string "card_number"
     t.decimal "amount"
-    t.date "card_expiry_date"
     t.string "card_cvv"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "card_expiry_month"
+    t.integer "card_expiry_year"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
